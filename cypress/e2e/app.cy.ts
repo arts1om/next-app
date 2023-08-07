bd068f38c61ac9ecd820191609a0cb41bfb5f464
artem.poddubitski <artem.poddubitski@solbeg.com>
2023-08-08 00:46:31 +0300

    add cypress

---
+++ cypress/e2e/app.cy.ts
@@ -0,0 +1,27 @@
+/* eslint-disable */
+// Disable ESLint to prevent failing linting inside the Next.js repo.
+// If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
+// https://github.com/cypress-io/eslint-plugin-cypress
+
+// Cypress E2E Test
+describe("Navigation", () => {
+  it("should navigate to the about page", () => {
+    // Start from the index page
+    cy.visit("http://localhost:3000/");
+
+    // The new page should contain an h1 with "About page"
+    cy.get("h2").contains("Home Page");
+
+    // Find a link with an href attribute containing "about" and click it
+    // cy.get('a[href*="about"]').click();
+
+    // The new url should include "/about"
+    // cy.url().should("include", "/about");
+
+    // The new page should contain an h1 with "About page"
+    cy.get("h2").should("be.visible");
+  });
+});
+
+// Prevent TypeScript from reading file as legacy script
+export {};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       {/* <body className={inter.className}>{children}</body> */}
-      <ThemeRegistry options={{ key: "mui" }}>{children}</ThemeRegistry>
+      <body>
+        <ThemeRegistry options={{ key: "mui" }}>{children}</ThemeRegistry>
+      </body>
     </html>
   );
 }
